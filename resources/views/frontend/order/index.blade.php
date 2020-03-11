1904c66ce03c3cbe4beac339e68f1bfd540fd402
Хомусяк Денис <homusyak.d@orionnet.ru>
2020-03-12 02:58:09 +0700

    add tabs to view

--- resources/views/frontend/order/index.blade.php
+++ resources/views/frontend/order/index.blade.php
@@ -1,5 +1,44 @@
 @extends('layouts.frontend')
 @section('content')
+
+    <ul class="nav nav-tabs">
+        <li>
+            <a href="{{route('order.index')}}"
+               class="nav-link @if(Request::has('type') === false) active @endif">
+                All
+            </a>
+        </li>
+        <li>
+            <a href="{{route('order.index', ['type' => 'past'])}}"
+               class="nav-link @if(Request::get('type') === 'past') active @endif">
+                Past
+            </a>
+        </li>
+        <li>
+            <a href="{{route('order.index', ['type' => 'current'])}}"
+               class="nav-link @if(Request::get('type') === 'current') active @endif">
+                Current
+            </a>
+        </li>
+        <li>
+            <a href="{{route('order.index', ['type' => 'new'])}}"
+               class="nav-link @if(Request::get('type') === 'new') active @endif">
+                New
+            </a>
+        </li>
+        <li>
+            <a href="{{route('order.index', ['type' => 'done'])}}"
+               class="nav-link @if(Request::get('type') === 'done') active @endif">
+                Done
+            </a>
+        </li>
+    </ul>
+    <div class="ibox-content">
+        <div class="tab-content">
+{{--                    @includeIf('frontend.address.includes._' . $view_page)--}}
+        </div>
+    </div>
+
     <table class="table table-bordered">
         <thead>
             <tr class="thead-dark">
@@ -35,7 +74,10 @@
         </tbody>
         <tfoot>
             <tr class="text-center">
-                <td colspan="5">{{ $orders->links() }}</td>
+                {{-- SO BAD --}}
+                @if (($orders instanceof \Illuminate\Support\Collection) === false)
+                    <td colspan="5">{{ $orders->links()  }}</td>
+                @endif
             </tr>
         </tfoot>
     </table>
